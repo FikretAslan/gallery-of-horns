b508945f59d068a0e28f29478c6bb13a0c4a16be
Fikret Aslan <f.aslan0191@gmail.com>
2023-10-23 14:12:51 +0100

    just checking

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        <h1>This is working!</h1>
+      </p>
+      <p>
+        <h1>This is working!</h1>
+      </p>
+      <p>
+        <h1>This is working!</h1>
+      </p>
+      <p>
+        <h1>This is working!</h1>
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,5 @@
+function Profile() {
+  return <p>Fikret is best</p>;
+}
+
+export default Profile();
